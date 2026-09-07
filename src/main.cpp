c9354d1d8adb8b2a069670cb3cc6c50f3d139339
Zhehang Chen <dimen10@student.ubc.ca>
2026-09-07 15:08:01 +0800

    Finish main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 #include "gemm/gemm.h"
+
+int main(){
+    int N = 4;
+    
+    float A[16];
+    float B[16];
+    float C[16];
+
+    for(int i = 0; i < 16; i++){
+        A[i] = 1.0;
+        B[i] = 2.0;
+        C[i] = 0;
+    }
+
+    naive_gemm(A, B, C, N);
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            std::cout << C[i*N + j] << " ";
+        }
+
+        std::cout<<std::endl;
+    }
+
+    return 0;
+}
