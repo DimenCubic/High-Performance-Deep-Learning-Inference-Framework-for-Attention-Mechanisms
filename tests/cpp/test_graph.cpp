9d0829c84c0fa60766621d0fe9aba4821a020340
Zhehang Chen <dimen10@student.ubc.ca>
2026-09-20 10:23:12 +0800

    finish topological sort test

--- tests/cpp/test_graph.cpp
+++ tests/cpp/test_graph.cpp
@@ -2,9 +2,30 @@
 #include "graph/graph.h"
 
 
-// Will do a QK to attention to softmax value 的 test case。
-int main(){
+
+void basic_test(){
     Graph graph;
+
+
+     graph.add_node(
+        Node(
+            "attention_scores",
+            "AttentionScores", 
+            {"Q", "K"},
+            {"scores"}
+        )
+    );
+
+
+    graph.add_node(
+        Node(
+            "softmax",
+            "Softmax", 
+            {"scores"},
+            {"weights"}
+        )
+    );
+
 
     graph.add_node(
         Node(
@@ -26,24 +47,8 @@ int main(){
     );
 
 
-    graph.add_node(
-        Node(
-            "attention_scores",
-            "AttentionScores", 
-            {"Q", "K"},
-            {"scores"}
-        )
-    );
-
-
-    graph.add_node(
-        Node(
-            "softmax",
-            "Softmax", 
-            {"scores"},
-            {"weights"}
-        )
-    );
+   
+    
 
 
     graph.build_dependencies();
@@ -68,5 +73,69 @@ int main(){
     }
 
 
+
+
+    // Test topological sort
+    std::vector<int> order = graph.topological_sort();
+
+    std::cout << "Execution Order:" << std::endl;
+    for(int index : order)
+        std::cout<<graph.nodes()[index].name() << std::endl;
+
+}
+
+
+
+
+void cycle_test(){
+    Graph graph;
+
+    graph.add_node(
+    Node(
+        "A",
+        "Test",
+        {"C_out"},
+        {"A_out"}
+    )
+    );
+
+    graph.add_node(
+        Node(
+            "B",
+            "Test",
+            {"A_out"},
+            {"B_out"}
+        )
+    );
+
+    graph.add_node(
+        Node(
+            "C",
+            "Test",
+            {"B_out"},
+            {"C_out"}
+        )
+    );
+
+    graph.build_dependencies();
+
+    try{
+        auto order = graph.topological_sort();
+    }catch(const std::runtime_error& e){
+        std::cout<<e.what()<<std::endl;
+    }
+
+
+}
+
+// Will do a QK to attention to softmax value 的 test case。
+int main(){
+    basic_test();
+
+    std::cout<<std::endl;
+    
+    cycle_test();
+    
+    
     return 0;
 }
